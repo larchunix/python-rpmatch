01930b41bdbd3a6895987c3c547ba511a4129b4c
Denis Ollier <larchunix@users.noreply.github.com>
2017-04-08 18:26:35 +0200

    setup: add installation script

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python -tt
+# -*- coding: UTF-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='rpmatch',
+    description='Python implementation of rpmatch(3) libc function',
+    author='Denis Ollier',
+    license='GPL-3.0-or-later',
+    version='1.0.0',
+    py_modules=['rpmatch'],
+)
+
+# vi: set et ft=python sw=4 ts=4:
