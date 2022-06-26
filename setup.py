7078eaae91a75eed2718c46f34adde21cfd94dad
Denis Ollier <larchunix@users.noreply.github.com>
2022-06-26 16:33:20 +0200

    setup: move to declarative setup.cfg

--- setup.py
+++ setup.py
@@ -6,21 +6,6 @@
 
 from setuptools import setup
 
-setup(
-  name='rpmatch',
-  description='Python implementation of rpmatch(3) libc function',
-  author='Denis Ollier',
-  license='GPL-3.0-or-later',
-  version='1.0.0',
-  classifiers=[
-    'Development Status :: 5 - Production/Stable',
-    'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-    'Operating System :: OS Independent',
-    'Programming Language :: Python',
-    'Topic :: Software Development :: Libraries :: Python Modules',
-  ],
-  py_modules=['rpmatch'],
-  python_requires='>=2.3',
-)
+setup()
 
 # vi: set ft=python et sw=4 ts=4:
